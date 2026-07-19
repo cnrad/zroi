d89bf1bea047ccc17f46fa0a43282f7754ca9f14
cnrad <conradwcrawford@gmail.com>
2026-07-19 01:03:08 -0400

    upgrade stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  compiler: {
+    // hand styled-components to the SWC transform so SSR class names match
+    styledComponents: true,
+  },
 }
